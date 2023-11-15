fd2c9a3f94b66c8b0841fcf57f632cbbf6c49b92
최지혁 <jihyeok@choejihyeog-ui-MacBookAir.local>
2023-11-15 17:03:16 +0900

    Initial commit

---
+++ app/src/main/java/com/team1/bohemian/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.team1.bohemian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
