a6f48a8fc2f688269569ef40514a58a22419a55c
Sardine2142 <98326930+Sardine2142@users.noreply.github.com>
2023-12-03 15:12:23 +0900

    리뷰를 구성하는 페이지를 차례로 등록하려고 하고 있습니다

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
